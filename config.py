045c64dcb17932c76e3fb4508bda03bc36929400
Francisco-1088 <32076369+Francisco-1088@users.noreply.github.com>
2024-03-14 11:38:31 -0400

    First commit

---
+++ config.py
@@ -0,0 +1,13 @@
+# Authentication Config
+api_key = 'YOUR-API-KEY'
+base_url = 'https://api.meraki.com/api/v1'
+
+# Orgs and Networks
+org_name = 'YOUR-ORG-NAME'
+net_names = ['NET-NAME-1', 'NET-NAME-2']
+
+# Log settings
+productType = ["switch", "wireless"]
+startingAfter = "2024-01-01T00:00:00"
+days_lookback = 30 # Used only if startingAfter is set to ""
+
---
+++ main.py
@@ -0,0 +1,35 @@
+import meraki
+import config
+import pandas as pd
+from datetime import datetime, timedelta
+
+dashboard = meraki.DashboardAPI(config.api_key)
+
+orgs = dashboard.organizations.getOrganizations()
+
+for org in orgs:
+    if org['name']==config.org_name:
+        org_id = org['id']
+
+nets = dashboard.organizations.getOrganizationNetworks(org_id)
+
+if config.startingAfter == "":
+    now = datetime.now()
+    startingAfter = (now - timedelta(days=config.days_lookback)).strftime("%Y-%m-%dT%H:%M:%S")
+else:
+    startingAfter = config.startingAfter
+
+for net_name in config.net_names:
+    net_id = ""
+    for net in nets:
+        if net['name']==net_name:
+            net_id = net['id']
+
+    if net_id != "":
+        for prod in config.productType:
+            try:
+                logs = dashboard.networks.getNetworkEvents(networkId=net_id, direction='prev', productType=prod, perPage=1000, startingAfter=startingAfter)
+                events_df = pd.DataFrame(logs['events'])
+                events_df.to_csv(f"{net_name}_{prod}_logs.csv")
+            except meraki.APIError as e:
+                print(e, net_name)
